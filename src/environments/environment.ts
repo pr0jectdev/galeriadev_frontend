fe0b593bffbd61e0b55f4613f1b9866f385712b9
pr0jectdev <ps4jogar3@gmail.com>
2026-06-24 18:01:51 -0300

    build: 2026.06.24.001 Alterado apiUrl

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,5 @@
 export const environment = {
   production: false,
-  apiUrl: 'http://localhost:5000/api'
+  apiUrl: 'https://galeriadev-backend.onrender.com/api'
+  //apiUrl: 'http://localhost:5000/api'
 };
